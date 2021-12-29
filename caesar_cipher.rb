61fed4064b262737ceb5e6539847eab4fcc4d573
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2021-12-29 12:18:07 -0500

    Clean up formatting

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -8,11 +8,11 @@ def caesar_cipher(string, shift_amount = 1)
   cipher = lowercase.merge(uppercase)
 
   encrypted_string = string.chars.map { |char| cipher.fetch(char, char) }.join
-  puts encrypted_string
 end
 
 puts "Please enter text to encrypt:"
 string = gets.chomp
 puts "Please enter a shift amount:"
 shift_amount = gets.chomp.to_i
-caesar_cipher(string, shift_amount)
+encrypted_string = caesar_cipher(string, shift_amount)
+puts "Encrypted string: #{encrypted_string}"
