9208b29fefd20fe6c15344e712f271981592b533
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2021-12-29 13:43:42 -0500

    Implement cipher function

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,18 +1,23 @@
-def caesar_cipher(string, shift_amount = 1)
-  alphabet = Array('a'..'z')
-  lowercase = Hash[alphabet.zip(alphabet.rotate(shift_amount))]
+def caesar_cipher(string, shift_amount)
 
-  alphabet = Array('A'..'Z')
-  uppercase = Hash[alphabet.zip(alphabet.rotate(shift_amount))]
+  def get_cipher(letters, shift_amount)
+    Hash[letters.zip(letters.rotate(shift_amount))]
+  end
 
-  cipher = lowercase.merge(uppercase)
-
-  encrypted_string = string.chars.map { |char| cipher.fetch(char, char) }.join
+  lowercase_letters = [*('a'..'z')]
+  uppercase_letters = [*('A'..'Z')]
+  lowercase_cipher = get_cipher(lowercase_letters, shift_amount)
+  uppercase_cipher = get_cipher(uppercase_letters, shift_amount)
+  cipher = lowercase_cipher.merge(uppercase_cipher)
+  string.chars.map {|char| cipher.fetch(char, char)}.join
 end
 
 puts "Please enter text to encrypt:"
 string = gets.chomp
+
 puts "Please enter a shift amount:"
 shift_amount = gets.chomp.to_i
+
 encrypted_string = caesar_cipher(string, shift_amount)
+
 puts "Encrypted string: #{encrypted_string}"
