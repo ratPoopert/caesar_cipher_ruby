e8b05dd86c6448e8b96627c774e6e00857353564
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2022-02-28 16:13:00 -0500

    Implement encrypt_string method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,23 +1,34 @@
 def caesar_cipher(string, shift_amount)
 
+  lowercase_letters = [*("a".."z")]
+  uppercase_letters = [*("A".."Z")]
+
   def get_cipher(letters, shift_amount)
     Hash[letters.zip(letters.rotate(shift_amount))]
   end
 
-  lowercase_letters = [*('a'..'z')]
-  uppercase_letters = [*('A'..'Z')]
-  lowercase_cipher = get_cipher(lowercase_letters, shift_amount)
-  uppercase_cipher = get_cipher(uppercase_letters, shift_amount)
+  def encrypt_string(string, cipher)
+    string.chars.map{|char| cipher.fetch(char, char)}.join
+  end
+  
+  lowercase_cipher = get_cipher(lowercase_letters, shift_amount) 
+  uppercase_cipher = get_cipher(uppercase_letters, shift_amount) 
   cipher = lowercase_cipher.merge(uppercase_cipher)
-  string.chars.map {|char| cipher.fetch(char, char)}.join
+
+  encrypted_string = encrypt_string(string, cipher)
+  
 end
 
-puts "Please enter text to encrypt:"
+puts "What is your name?"
+name = gets.chomp
+
+puts "Hello, #{name}! I'd like to encrypt a message for you!"
+puts "Please enter a message for me to encrypt:"
 string = gets.chomp
 
-puts "Please enter a shift amount:"
+puts "Thanks for the message! I'll also need a shift amount for the cipher"
+puts "For example, a shift of 3 would make A into D, B into E, etc."
+puts "Please enter a shift amount now:"
 shift_amount = gets.chomp.to_i
-
 encrypted_string = caesar_cipher(string, shift_amount)
-
-puts "Encrypted string: #{encrypted_string}"
+puts "Done! Your encrypted message is '#{encrypted_string}'."
